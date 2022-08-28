32c486e9d23a10f41975bcca7998c4b41467a1d0
k-nishitani <cxknishitani@gmail.com>
2022-08-29 01:02:08 +0900

    PJ追加

---
+++ App/vue_app/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="aboutMap">
+        <!--マップコンポーネント表示-->
+        <MapPane></MapPane>
+    </div>
+</template>
+
+<script>
+// マップコンポーネント読み込み
+    import MapPane from '@/components/MapPane.vue'
+
+    export default {
+        name: 'aboutMap',
+        components: {
+            MapPane
+        }
+    }
+</script>
