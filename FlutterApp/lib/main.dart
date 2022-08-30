1d269b1d57fcc9fa55e437bbb31cb74ce9cf8350
k-nishitani <cxknishitani@gmail.com>
2022-08-30 18:11:46 +0900

    地図表示＆マーカ配置

--- FlutterApp/lib/main.dart
+++ FlutterApp/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:positioned_tap_detector_2/positioned_tap_detector_2.dart';
 
 void main() {
   runApp(const MyApp());
@@ -48,68 +51,88 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  List<Marker> markers = <Marker>[
+    Marker(
+      point: LatLng(34.6857615848013, 135.52573444889032), //場所
+      builder: (ctx) => GestureDetector(
+        onTap: () {
+          ScaffoldMessenger.of(ctx).showSnackBar(const SnackBar(
+            content: Text("大阪城"),
+          ));
+        },
+        child: const Icon(
+          Icons.castle,
+          color: Colors.redAccent,
+        ),
+      ),
+    ),
+    Marker(
+      point: LatLng(34.702749991520044, 135.4958862250534), //場所
+      builder: (ctx) => GestureDetector(
+        onTap: () {
+          ScaffoldMessenger.of(ctx).showSnackBar(const SnackBar(
+            content: Text("大阪駅"),
+          ));
+        },
+        child: const Icon(
+          Icons.train,
+        ),
+      ),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      appBar: AppBar(title: const Text('テスト')),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(top: 8, bottom: 8),
+              child: Text('Try tapping on the markers'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Flexible(
+              child: FlutterMap(
+                  options: MapOptions(
+                      center: LatLng(34.6857615848013, 135.52573444889032),
+                      zoom: 13,
+                      onTap: _handleTap),
+                  layers: [
+                    TileLayerOptions(
+                      urlTemplate:
+                          'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                      subdomains: ['a', 'b', 'c'],
+                      userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+                    ),
+                    MarkerLayerOptions(markers: markers),
+                  ]),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void _handleTap(TapPosition tapPosition, LatLng latlng) {
+    setState(() {
+      markers.add(Marker(
+        point: latlng,
+        builder: (ctx) => GestureDetector(
+          onTap: () {
+            ScaffoldMessenger.of(ctx).showSnackBar(SnackBar(
+                content: Text('地点 (' +
+                    latlng.latitude.toStringAsPrecision(4) +
+                    "." +
+                    latlng.longitude.toStringAsPrecision(4) +
+                    ')')));
+          },
+          child: const Icon(
+            Icons.place,
+          ),
+        ),
+      ));
+    });
   }
 }
